106862cb87cfd6de7629aa1159d35745697b9eac
yuriy <yuriy.menkov@latoken.com>
2019-11-13 21:58:12 +0300

    Fix wallet

--- frontend/src/vuex/modules/wallet.ts
+++ frontend/src/vuex/modules/wallet.ts
@@ -4,6 +4,7 @@ import axios from "axios"
 import { fetchAccount } from "../../mock-service"
 import { TNode } from "@/connectors/node"
 import { Module } from "vuex"
+import Tendermint from "@/connectors/tendermint"
 
 const emptyState = {
   balances: Array<any>(),
@@ -12,7 +13,7 @@ const emptyState = {
   error: null,
   accountNumber: null,
   address: null,
-  subscribedRPC: null,
+  subscribedRPC: {} as Tendermint,
   externals: { config, axios }
 }
 
@@ -87,8 +88,37 @@ export default ({ node }: { node: TNode }): Module<typeof emptyState, any> => ({
         state.error = error
       }
     },
-    queryWalletStateAfterHeight() {},
-    async walletSubscribe() {}
+    queryWalletStateAfterHeight({ rootState, dispatch }, height) {
+      return new Promise(resolve => {
+        // wait until height is >= `height`
+        const interval = setInterval(() => {
+          if (rootState.connection.lastHeader.height < height) return
+          clearInterval(interval)
+          dispatch(`queryWalletBalances`)
+          dispatch(`getBondedDelegates`) // TODO move away
+          dispatch(`getRewardsFromMyValidators`) // TODO move away
+          resolve()
+        }, 1000)
+      })
+    },
+    async walletSubscribe({ state, rootState, dispatch }) {
+      if (!rootState.session.address) return
+      // check if we already subscribed to this rpc object
+      // we need to resubscribe on rpc reconnections
+      if (state.subscribedRPC === node.tendermint) return
+
+      state.subscribedRPC = node.tendermint;
+
+      try {
+        await subscribeToTxs(
+          node.tendermint,
+          rootState.session.address,
+          dispatch
+        )
+      } catch (error) {
+        state.error = error
+      }
+    }
   }
 })
 
@@ -98,101 +128,21 @@ export default ({ node }: { node: TNode }): Module<typeof emptyState, any> => ({
 //   return acc
 // }, {})
 
-// actions: {
-//   async reconnected({ rootState, state, dispatch }) {
-//     if (state.loading && state.address && rootState.session.signedIn) {
-//       await dispatch(`queryWalletBalances`)
-//     }
-//   },
-//   async initializeWallet({ commit, dispatch }, { address }) {
-//     commit(`setWalletAddress`, address)
-//
-//     dispatch(`queryWalletBalances`)
-//
-//     // dispatch(`walletSubscribe`)
-//
-//     // await dispatch(`getBondedDelegates`) // TODO move away
-//     // dispatch(`getRewardsFromMyValidators`) // TODO move away
-//   },
-//   resetSessionData({ rootState }) {
-//     // clear previous account state
-//     rootState.wallet = JSON.parse(JSON.stringify(emptyState))
-//     rootState.wallet.externals.axios = axios
-//   },
-//   async queryWalletBalances({ state, rootState, commit }) {
-//     if (!state.address) return
-//
-//     state.loading = true
-//     if (!rootState.connection.connected) return
-//
-//     try {
-//       // const res = await node.get.account(state.address)
-//
-//       // Mock account data
-//       const { value: res } = await fetchAccount(state.address)
-//
-//       state.error = null
-//       const { coins, account_number } = res || {}
-//       commit(`setAccountNumber`, account_number)
-//       commit(`setWalletBalances`, coins || [])
-//       state.loading = false
-//       state.loaded = true
-//     } catch (error) {
-//       commit(`notifyError`, {
-//         title: `Error fetching balances`,
-//         body: error.message
-//       })
-//       state.error = error
-//     }
-//   },
-//   queryWalletStateAfterHeight({ rootState, dispatch }, height) {
-//     return new Promise(resolve => {
-//       // wait until height is >= `height`
-//       const interval = setInterval(() => {
-//         if (rootState.connection.lastHeader.height < height) return
-//         clearInterval(interval)
-//         dispatch(`queryWalletBalances`)
-//         dispatch(`getBondedDelegates`) // TODO move away
-//         dispatch(`getRewardsFromMyValidators`) // TODO move away
-//         resolve()
-//       }, 1000)
-//     })
-//   },
-//   async walletSubscribe({ state, rootState, dispatch }) {
-//     if (!rootState.session.address) return
-//     // check if we already subscribed to this rpc object
-//     // we need to resubscribe on rpc reconnections
-//     if (state.subscribedRPC === node.tendermint) return
-//
-//     state.subscribedRPC = node.tendermint
-//
-//     try {
-//       await subscribeToTxs(
-//         node.tendermint,
-//         rootState.session.address,
-//         dispatch
-//       )
-//     } catch (error) {
-//       state.error = error
-//     }
-//   }
-// }
+function subscribeToTxs(tendermint: Tendermint, address: string, dispatch: any) {
+  function onTx(data: any) {
+    dispatch(`queryWalletStateAfterHeight`, data.TxResult.height + 1)
+  }
 
-// function subscribeToTxs(tendermint, address, dispatch) {
-//   function onTx(data) {
-//     dispatch(`queryWalletStateAfterHeight`, data.TxResult.height + 1)
-//   }
-//
-//   const queries = [
-//     `tm.event = 'Tx' AND sender = '${address}'`,
-//     `tm.event = 'Tx' AND recipient = '${address}'`,
-//     `tm.event = 'Tx' AND proposer = '${address}'`,
-//     `tm.event = 'Tx' AND depositor = '${address}'`,
-//     `tm.event = 'Tx' AND delegator = '${address}'`,
-//     `tm.event = 'Tx' AND voter = '${address}'`
-//   ]
-//
-//   return Promise.all(
-//     queries.map(query => tendermint.subscribe({ query }, onTx))
-//   )
-// }
+  const queries = [
+    `tm.event = 'Tx' AND sender = '${address}'`,
+    `tm.event = 'Tx' AND recipient = '${address}'`,
+    `tm.event = 'Tx' AND proposer = '${address}'`,
+    `tm.event = 'Tx' AND depositor = '${address}'`,
+    `tm.event = 'Tx' AND delegator = '${address}'`,
+    `tm.event = 'Tx' AND voter = '${address}'`
+  ]
+
+  return Promise.all(
+    queries.map(query => tendermint.subscribe({ query }, onTx))
+  )
+}

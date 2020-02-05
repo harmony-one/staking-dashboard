3629c5dd0be59c9d48cf74da2313c4220658f237
Minh Doan <daywednes@gmail.com>
2020-02-05 12:09:09 -0800

    update the current networks and readme

--- appengine/mock-data/networks.js
+++ appengine/mock-data/networks.js
@@ -1,5 +1,5 @@
-const HmyMainnet = 1;
-const HmyTestnet = 2;
+const HmyMainnet = 1
+const HmyTestnet = 2
 
 module.exports = [
   {
@@ -31,8 +31,19 @@ module.exports = [
     logo_url: "https://harmony.one/logo",
     testnet: true,
     title: "Harmony Testnet Local",
-    rpc_url: 'http://localhost:9500',
+    rpc_url: "http://localhost:9500",
     explorer_url: "http://localhost:9500/#/tx/",
     __typename: "networks"
+  },
+  {
+    id: "harmony-open-staking",
+    chain_id: HmyTestnet,
+    chain_title: "Openstakingnet",
+    logo_url: "https://harmony.one/logo",
+    testnet: true,
+    title: "Harmony Open Staking",
+    rpc_url: "http://api.s0.os.hmny.io",
+    explorer_url: "https://explorer.os.hmny.io/#/tx/",
+    __typename: "networks"
   }
-];
+]

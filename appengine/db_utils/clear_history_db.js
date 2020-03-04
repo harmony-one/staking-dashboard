51430feaa2c9de48a4cbfe78993364d692ed1a7e
Minh Doan <daywednes@gmail.com>
2020-03-04 12:17:50 -0800

    add prefix

--- appengine/db_utils/clear_history_db.js
+++ appengine/db_utils/clear_history_db.js
@@ -1,7 +1,8 @@
 const admin = require('firebase-admin')
 var serviceAccount = require('../keys/staking_explorer.json')
 
-const DATABASE_URL = process.env.DATABASE_URL || 'https://staking-explorer.firebaseio.com'
+const DATABASE_URL =
+  process.env.DATABASE_URL || 'https://staking-explorer.firebaseio.com'
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -9,20 +10,24 @@ admin.initializeApp({
 })
 
 const db = admin.firestore()
+const DELETED_COLLECTION_SUFFIX = '_history'
 
-const removeCollection = (collectionId) => {
-  return db.collection(collectionId).listDocuments().then(val => {
-    console.log('Remove: ', collectionId)
+const removeCollection = collectionId => {
+  return db
+    .collection(collectionId)
+    .listDocuments()
+    .then(val => {
+      console.log('Remove: ', collectionId)
 
-    return Promise.all(val.map(val => val.delete()))
-  })
+      return Promise.all(val.map(val => val.delete()))
+    })
 }
 
 db.listCollections().then(res => {
   res.reduce((asyncRes, collection) => {
     const { collectionId } = collection._queryOptions
 
-    if (collectionId.indexOf('_history') > -1) {
+    if (collectionId.includes(DELETED_COLLECTION_SUFFIX)) {
       return asyncRes
         .then(() => removeCollection(collectionId))
         .catch(err => console.log('Error ', collectionId, err))

fe54a55d40b863de08bf59827030129b001c815c
yuriy <yuriy.menkov@latoken.com>
2020-03-04 22:33:42 +0300

    Added async sequential documents removing

--- appengine/db_utils/clear_history_db.js
+++ appengine/db_utils/clear_history_db.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin')
-var serviceAccount = require('./keys/staking_explorer.json')
+var serviceAccount = require('../keys/staking_explorer.json')
 
 const DATABASE_URL = process.env.DATABASE_URL || 'https://staking-explorer.firebaseio.com'
 
@@ -11,19 +11,21 @@ admin.initializeApp({
 const db = admin.firestore()
 
 const removeCollection = (collectionId) => {
-  db.collection(collectionId).listDocuments().then(val => {
-    Promise.all(val.map(val => val.delete()))
+  return db.collection(collectionId).listDocuments().then(val => {
+    console.log('Remove: ', collectionId)
+
+    return Promise.all(val.map(val => val.delete()))
   })
 }
 
 db.listCollections().then(res => {
-  res.forEach(collection => {
+  res.reduce((asyncRes, collection) => {
     const { collectionId } = collection._queryOptions
 
     if (collectionId.indexOf('_history') > -1) {
-      console.log('Remove: ', collectionId)
-
-      removeCollection(collectionId)
+      return asyncRes.then(() => removeCollection(collectionId))
     }
-  })
+
+    return asyncRes
+  }, Promise.resolve())
 })

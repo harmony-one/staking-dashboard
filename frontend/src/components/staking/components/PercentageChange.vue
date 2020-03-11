5bf8731cd851cf22aa9e25d05e1f501494fd8dc2
yuriy <yuriy.menkov@latoken.com>
2020-03-11 18:46:48 +0300

    #125 Validator list page: show median and total stake amount percentage change from last epoch in brackets next to the Total stake and Median stake numbers.

---
+++ frontend/src/components/staking/components/PercentageChange.vue
@@ -0,0 +1,54 @@
+<template>
+  <span :class="percentageChangeClass">
+    <i>{{ sign }}</i
+    >{{ amount | percent }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: `PercentageChange`,
+  filters: {
+    percent: value =>
+      new Intl.NumberFormat("en-US", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 4
+      }).format(Math.round(value * 10000) / 10000) + `%`
+  },
+  props: {
+    amount: {
+      type: Number,
+      default: () => 0
+    }
+  },
+  computed: {
+    sign() {
+      return this.amount >= 0 ? "+" : ""
+    },
+    percentageChangeClass() {
+      return {
+        percentageChangeContainer: true,
+        red: this.amount < 0,
+        green: this.amount > 0
+      }
+    }
+  }
+}
+</script>
+<style lang="scss">
+.percentageChangeContainer {
+  font-size: 14px;
+
+  i {
+    font-size: 16px;
+  }
+
+  &.red {
+    color: var(--red);
+  }
+
+  &.green {
+    color: var(--success);
+  }
+}
+</style>

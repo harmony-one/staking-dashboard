9b466067584069af520b6b8f99ce0fdae7583b6f
Matt Lockyer <mattdlockyer@gmail.com>
2020-03-17 16:21:15 -0700

    update allstakeschart

--- frontend/src/components/staking/AllStakesChart.vue
+++ frontend/src/components/staking/AllStakesChart.vue
@@ -1,7 +1,8 @@
+
 <template>
   <div>
     <div class="chart-container">
-      <ChartLine
+      <ChartBar
         :chartdata="chartdata"
         :options="options"
         style="height: 300px; width: 100%;"
@@ -11,6 +12,7 @@
 </template>
 
 <script>
+<<<<<<< HEAD
 import ChartLine from "./PageValidatorCharts/components/ChartLine"
 import { ones, zeroDecimals } from "../../scripts/num"
 
@@ -18,9 +20,13 @@ import { ones, zeroDecimals } from "../../scripts/num"
 //   return Math.round(Number(min) + Math.random() * (number || 100))
 // }
 
+=======
+import ChartBar from "./PageValidatorCharts/components/ChartBar"
+import { ones, zeroDecimals } from "../../scripts/num"
+>>>>>>> update allstakeschart
 export default {
   name: "AllStakesChart",
-  components: { ChartLine },
+  components: { ChartBar },
   props: ["data"],
   data: () => ({
     options: {
@@ -32,7 +38,22 @@ export default {
         callbacks: {
           title: (data) => "",
           label: (data, a, b) => 
+<<<<<<< HEAD
             zeroDecimals(data.yLabel) + " ONE Staked"// by " + a.datasets[0].pointRadius({dataIndex: data.index})
+=======
+            zeroDecimals(data.yLabel) + " ONE Staked"
+        }
+      },
+      plugins: {
+        labels: {
+          render: () => {}
+        }
+      },
+      ticks: {
+        afterUpdate: () => {
+          const el = document.querySelector('#bar-chart')
+          console.log(el)
+>>>>>>> update allstakeschart
         }
       },
       // hover: {
@@ -44,7 +65,7 @@ export default {
           {
             display: true,
             gridLines: {
-              display: true
+              display: false
             }
           }
         ],
@@ -61,8 +82,19 @@ export default {
   }),
   computed: {
     chartdata() {
+<<<<<<< HEAD
       // const raw = this.data.map((v) => ones(v))
       // const cache = []
+=======
+      const raw = this.data.map((v) => ones(v))
+      const cache = []
+      // previous
+      const data = raw.slice().reverse()
+      const median = data[Math.floor(data.length / 2)]
+      console.log(median)
+      
+      // new
+>>>>>>> update allstakeschart
       // const data = raw.sort((a, b) => a - b).filter((value) => {
       //   const cached = cache.find((d) => d.value === value)
       //   if (!cached) {
@@ -74,15 +106,25 @@ export default {
       //   return false
       // })
       // const count = cache.map((c) => c.count)
+<<<<<<< HEAD
       
       const data = this.data.map((v) => ones(v))
       const labels = data.map((v, idx) => idx)
 
+=======
+      const labels = data.map((v, idx) => idx + 1)
+>>>>>>> update allstakeschart
       return {
         labels,
         datasets: [
           {
+            barPercentage: 1.01,
+            categoryPercentage: 1.01,
+            // barThickness: 6,
+            // maxBarThickness: 8,
+            minBarLength: 8,
             label: "Staked ONE distribution",
+<<<<<<< HEAD
             borderColor: "#0a93eb",
             borderWidth: 2,
             fill: false,
@@ -90,6 +132,9 @@ export default {
             // pointRadius: function(context) {
             //   return count[context.dataIndex]
             // },
+=======
+            backgroundColor: "#0a93eb",
+>>>>>>> update allstakeschart
             data
           }
         ]

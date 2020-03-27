43be6820fe902a20308a500d9d4e1774090a13b2
Matt Lockyer <mattdlockyer@gmail.com>
2020-03-26 19:12:49 -0700

    update slots

--- frontend/src/components/staking/PageValidatorCharts/PerfomanceBlock.vue
+++ frontend/src/components/staking/PageValidatorCharts/PerfomanceBlock.vue
@@ -18,7 +18,7 @@
         <span>{{ validator.active_nodes || 0 }}</span>
       </li>
       <li class="row">
-        <h4>Elected Nodes</h4>
+        <h4>Elected Slots</h4>
         <span>{{ validator.elected_nodes || 0 }}</span>
       </li>
       <li class="row">

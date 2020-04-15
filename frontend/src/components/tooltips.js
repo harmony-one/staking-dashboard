af3d8a7199979193a517b33a8511cfd83abbd05a
Matt Lockyer <mattdlockyer@gmail.com>
2020-04-14 21:29:44 -0700

    update tooltips

--- frontend/src/components/tooltips.js
+++ frontend/src/components/tooltips.js
@@ -1,74 +1,80 @@
 export default {
   portfolio: {
     total_one:
-      "Current available balance for the account on Shard 0 + total amount staked in all validators + rewards available to collect",
-    staked: "Total amount delegated to validators",
-    available: "Current available balance for the account on Shard 0",
-    rewards: "Unclaimed rewards for the account",
+      "Total ONE tokens",
+    staked: "Amount of ONE delegated",
+    available: "Amount of ONE that available to delegate",
+    rewards: "Rewards yet to be claimed",
     lifetime_rewards:
-      "Total rewards earned by the account (claimed + unclaimed)",
+      "Total lifetime rewards earned",
     portfolio_allocation:
-      "Distribution of delegations by this account across different validators",
+      "Delegation amount across different validators",
 
     // delegation table
     status: `The validator's status`,
     name: `The validator's moniker`,
     stake: `Stake of validator`,
     reward_up_to_date:
-      "Total rewards earned from each validator (claimed + unclaimed)",
+      "Unclaimed rewards",
     apr_avg: "Average annual percentage return rate from each validator",
     ending_in:
-      "Epochs left until undelegation, funds will not be accessible until then"
+      "Epochs left until undelegation funds will be accessible"
   },
 
   v_list: {
     effective_median_stake:
-      "Median of amount staked among the top elected slots",
-    total_stake: "Total amount staked in the selected Harmony network",
+      "Median of ONE staked among the top elected slots ",
+    total_stake: "Total ONE staked in the Harmony network",
+    current_block_number: "Current block height of Harmony blockchain",
+
     all: "All validators that have been created & listed on the chain",
 
     // status cell
     elected:
-      "Validator selected in the current committe that signs blocks & collects rewards (does not indicate that your node is actually signing blocks)",
+      "Validator currently selected & eligible to sign blocks & earn rewards",
     not_elected:
-      "Validator information exists on chain, but has not been selected as part of the current committee (can be due to low stake, unavailibility, banned)",
+      "Validator currently not-selected to sign blocks due to low stake or insufficient uptime",
 
     // table list
-    status: `The validator's status`,
-    name: `The validator's moniker`,
-    apr: "Expected annual percentage return rate for staked amount",
-    stake: "Total stake of the validator",
+    status: `Election status of validator in current epoch`,
+    name: `Validator name`,
+    apr: "Expected annual percentage return rate",
+    stake: "Total ONE staked by the validator",
     fees:
-      "Commission rate that the validator charges on the rewards earned from delegations",
+      "Commission on rewards charged by validator",
     uptime:
-      "Num blocks signed / Num blocks able to have signed (Lifetime stats)"
+      "Signing percentage for this validator's nodes"
   },
 
   v_profile: {
-    total_staked: "Total amount staked on this validator",
-    delegated: "Total amount delegated to this validator",
-    self_stake: "Amount of stake that the validator delegated to itself",
+    total_staked: "Total ONE staked on this validator",
+    delegated: "Total ONE delegated to this validator",
+    self_stake: "Amount of ONE staked by validator ",
     max_delegation:
-      "Maximum amount of total stake allowed by the validator, including self-stake",
+      "Maximum ONE stake allowed by the validator, including self-stake",
     validator_since: "Block number at which validator registered",
     commission:
-      "Commission rate that the validator charges on the rewards earned from delegations",
+      "Commission on rewards charged by validator",
     max_daily_change:
-      "Maximum daily percent point change allowed for this validator",
+      "Maximum daily change in commission allowed for this validator",
     uptime_avg:
-      "Num blocks signed / Num blocks able to have signed (Lifetime stats)",
+      "Signing percentage for this validator's nodes",
     slots:
-      "Number of BLS Keys associated with the Validator (Does not indicate number of nodes the Validator is running)",
+      "Number of seats (bls keys) associated with the Validator",
     elected_slots:
-      "Number of BLS keys assigned a slot in the current committee by this validator",
+      "Number of seats (bls keys) elected in the current committee",
     apr: "Average estimated annual percentage return rate by this validator",
     shards:
-      "Shards in which validator's BLS keys belong, shown order is based on time to add (shards: 0, 1, 2, 3)",
+      "Shards in which validator's BLS keys belong, shown order is based on time to add",
     stake_delegation_history:
-      "Stake and delegation amount by the validator at every epoch",
+      "Stake and delegation amount for the validator at every epoch",
     reward_rate_history:
-      "Expected annual percentage return rate for staked amount at every epoch",
+      "Expected annual percentage return rate for validator at every epoch",
     delegators:
-      "List of accounts that delegated to this validator and delegation amount, including pending undelegations"
+      "List of accounts that delegated to this validator and delegation amount"
+  }
+
+  v_global: {
+    
   }
 }

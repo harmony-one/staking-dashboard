eddba148d2c6a6316cf1d62bfd4f06ebcf26174e
yuriy <yuriy.menkov@latoken.com>
2020-05-21 21:56:54 +0300

    remove Tendermint

--- frontend/src/connectors/node.ts
+++ frontend/src/connectors/node.ts
@@ -1,18 +1,15 @@
-import Tendermint from "./tendermint"
 import Staking from "@/staking-client/Staking"
 import Legacy from "@/staking-client/Legacy"
 
-export type TNode = { tendermint: Tendermint, get: Legacy, staking: Staking };
+export type TNode = { tendermint: any, get: Legacy, staking: Staking };
 
 export default function Connector(apiUrl: string): TNode {
   const stakingClient = new Staking();
   const legacyClient = new Legacy(apiUrl);
 
-  const tendermint = new Tendermint(apiUrl);
-
   return Object.assign({
     staking: stakingClient,
     get: legacyClient,
-    tendermint,
+    tendermint: {},
   });
 }

cd8a3a2602238f49fa16147ea4129c56444fbb5f
jenya <hypnagonia@gmail.com>
2020-08-06 10:10:29 +0300

    WIP fixed cache update

--- appengine/src/services/sync/validators-avatars.ts
+++ appengine/src/services/sync/validators-avatars.ts
@@ -6,34 +6,29 @@ const INTERVAL = 1000 * 60 * 60
 export class ValidatorsAvatarCacheService {
     cache = {
         AVATAR_URLS: {},
-        lastCachedDate: 0
     }
     timeoutId = null
 
 
     cacheAvatars = async (validators) => {
-        // if cache has not expired
-        if (this.cache.lastCachedDate && (Date.now() - this.cache.lastCachedDate < INTERVAL)) {
-            clearTimeout(this.timeoutId)
-            this.timeoutId = setTimeout(
-                () => this.cacheAvatars(validators),
-                INTERVAL - (Date.now() - this.cache.lastCachedDate))
-
+        if (this.timeoutId) {
             return
         }
 
         for (let v of validators) {
-            // console.log({v})
-            // await get image by url
-            // cache image
-            const githubAvatar = await this.fetchGithubAvatarByValidatorAddress(v.address)
-            const keyBaseAvatar = await this.fetchKeyBaseAvatarByValidatorIdentity(v.identity)
-            this.cache.AVATAR_URLS[v.address] = {githubAvatar, keyBaseAvatar}
+            try {
+                const githubAvatar = await this.fetchGithubAvatarByValidatorAddress(v.address)
+                const keyBaseAvatar = await this.fetchKeyBaseAvatarByValidatorIdentity(v.identity)
+                this.cache.AVATAR_URLS[v.address] = {githubAvatar, keyBaseAvatar}
+            } catch (e) {
+            }
         }
 
-        this.cache.lastCachedDate = Date.now()
         this.timeoutId = setTimeout(
-            () => this.cacheAvatars(validators),
+            () => {
+                clearTimeout(this.timeoutId)
+                this.cacheAvatars(validators)
+            },
             INTERVAL)
     }
 

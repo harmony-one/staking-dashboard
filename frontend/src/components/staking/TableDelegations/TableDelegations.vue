297892d0c07dd86437604e69e6414a4c6d7073d7
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2021-11-11 08:46:14 +0100

    [FIX] parseFloat to remove decimals when = 0.000000 ...

--- frontend/src/components/staking/TableDelegations/TableDelegations.vue
+++ frontend/src/components/staking/TableDelegations/TableDelegations.vue
@@ -122,7 +122,7 @@ export default {
             tooltip: tooltips.portfolio.reward_up_to_date,
             width: "200px",
             align: "right",
-            render: value => ones(value).toFixed(3) + " ONE" 
+            render: value => parseFloat(ones(value).toFixed(3)) + " ONE" 
           },
           {
             title: `Expected Return`,

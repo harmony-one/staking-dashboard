7df4adca9b887b40c4424d06843eefa83c814e99
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2021-11-11 08:37:14 +0100

    [UPD] Decimals to Delegations Table

--- frontend/src/components/staking/TableDelegations/TableDelegations.vue
+++ frontend/src/components/staking/TableDelegations/TableDelegations.vue
@@ -122,7 +122,7 @@ export default {
             tooltip: tooltips.portfolio.reward_up_to_date,
             width: "200px",
             align: "right",
-            render: value => zeroDecimals(ones(value)) + " ONE"
+            render: value => ones(value).toFixed(3) + " ONE" 
           },
           {
             title: `Expected Return`,

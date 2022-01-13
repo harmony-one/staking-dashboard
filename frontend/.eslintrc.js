11227fdee5db536ff412aa72a299300ce1fd374c
ahiipsa <ahiipsa@gmail.com>
2022-01-13 22:01:26 +0300

    Fixed eslint and prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,7 +10,10 @@ module.exports = {
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prefer-const": "off",
+    "no-var": "off",
+    "vue/no-unused-components": "off"
   },
 
   parserOptions: {
@@ -20,6 +23,7 @@ module.exports = {
   'extends': [
     'plugin:vue/recommended',
     '@vue/prettier',
-    '@vue/typescript'
+    '@vue/typescript',
+    'prettier'
   ]
 };
